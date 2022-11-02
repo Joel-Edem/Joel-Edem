891e22c67120003b3f7ec5914db0bcbe11a753dc
Joel-Edem <joelametepeh@gmail.com>
2022-11-02 03:29:38 +0000

    modified animations and css

--- js/main.js
+++ js/main.js
@@ -9,56 +9,61 @@ const welcome_tl = gsap.timeline({
   scrollTrigger: {
     trigger: "#welcome-section",
     pin: "#welcome-section",
-    preventOverlaps: true,
-    fastScrollEnd: true,
+    // preventOverlaps: true,
+    // fastScrollEnd: true,
     start: "top top",
-    pinSpacing: false
+    pinSpacing: false,
   }
 })
   .from("#blob",   // BLOB ANIMATION
-  {
-    y: "-100vh", onComplete: () => {
-      console.log("complete")
-      blob_anim.start()
-      console.log('starting')
-    }, scale: 1, duration: 2.5, ease: 'back'
-  }
-).fromTo(document.querySelectorAll(".content-body"), // WELCOME SECTION TEXT ANIMATION
-  {y: "-100%", opacity: 0},
-  {y: 0, opacity: "100%", duration: 2, ease: "power2.easeIn"}, "<+.5")
+    {
+      y: "-100vh", onComplete: () => {
+        console.log("complete")
+        blob_anim.start()
+        console.log('starting')
+      }, scale: 1, duration: 2.5, ease: 'back'
+    }
+  ).fromTo(document.querySelectorAll(".content-body"), // WELCOME SECTION TEXT ANIMATION
+    {y: "-100%", opacity: 0},
+    {y: 0, opacity: 1, duration: 2, ease: "power2.easeIn"}, "<+.5")
   .fromTo(document.querySelectorAll(".content-title"),
     {opacity: 0, x: "-30%"},
-    {opacity: "100%", x: 0, duration: 2, ease: "power2"}, "<+.5")
-
+    {opacity: 1, x: 0, duration: 2, ease: "power2"}, "<+.5")
 
 
 const phone_el = document.querySelector("#iphone")
 const icon_parent_el = phone_el.querySelector(".icon-parent")
 
-const phone_pin = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#phone-section",
-    start: "top top",
-    pin: "#phone-section",
-    pinSpacing: false,
-    endTrigger: "#hm-section",
-    end: "top top",
-  }
-})
-
-const phone_tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#phone-section",
-    start: "top top",
-    toggleActions: "play none none reverse",
-
-  }
-})
-  .from(phone_el, {rotate: 90, ease: "back", duration: 1, scale:.8})
+const phone_pin = ScrollTrigger.create({
+  trigger: "#phone-section",
+  start: "top top",
+  pin: "#phone-section",
+  pinSpacing: false,
+  endTrigger: "#hm-section",
+  end: "top top",
+  animation:gsap.timeline()
+  .fromTo(phone_el, {rotate: 90, ease: "back", duration: 1, scale:.8}, {rotate: 0,scale:1})
   .from(phone_el.querySelectorAll(".device-icon"),
     {rotate: -90, transformOrigin: "50% 50%", ease: "back", duration: 1}, "<+.1")
-  .to('.iphone-content', { opacity:1, duration: .5}, "<-.1")
-  .to("#iphone", {scale: .9, ease: "power2", duration: 1.5,}, "<.2")
+  .fromTo('.iphone-content', { opacity:0, y:"50vh",duration: 1}, { opacity:1, y:"0" },"<-.1")
+  // .to("#iphone", {scale: .9, ease: "power2", duration: 1.5,}, "<.2")
+  //
+
+})
+//
+// const phone_tl = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: "#phone-section",
+//     start: "top top",
+//     toggleActions: "play none none reverse",
+//
+//   }
+// })
+//   .from(phone_el, {rotate: 90, ease: "back", duration: 1, scale:.8})
+//   .from(phone_el.querySelectorAll(".device-icon"),
+//     {rotate: -90, transformOrigin: "50% 50%", ease: "back", duration: 1}, "<+.1")
+//   .to('.iphone-content', { opacity:1, duration: .5}, "<-.1")
+//   .to("#iphone", {scale: .9, ease: "power2", duration: 1.5,}, "<.2")
 
 
 const phone_scroll_tl = ScrollTrigger.create(
@@ -90,13 +95,14 @@ const imac_trigger = ScrollTrigger.create(
     toggleActions: "play none none reset",
     animation: gsap.timeline().to("#iphone", {scale: .8, opacity: 0, duration: .3})
       .fromTo(imac,
-        {scale: 1.1, opacity: 0, top: "-10%", duration: .3},
-        {scale: .9, opacity: 1, top: "10%"}
+        {scale: 1.1, opacity: 0,  duration: .3},
+        {scale: .9, opacity: 1, }
       )
-      .fromTo(".iphone-content", {scale: 1, opacity: 1,}, {scale: 0, opacity: 0, duration: .3,}, "<-.3")
+      .fromTo(".iphone-content", {scale: 1, opacity: 1,}, {scale: .5, opacity: 0, duration: .3,}, "<")
+      .fromTo(imac,{y: "10%",  duration: 1}, {y: "25%", }, ">")
       .fromTo(".imac-content ", {y: "-50%", opacity: 0}, {y: 0, opacity: 1})
       .fromTo(imac.querySelectorAll(".device-icon"),
-        {opacity: 0, y: 10}, {stagger: {each: 0.1}, opacity: 100, y: 0,}, "<"),
+        {opacity: 0, y: 10}, {stagger: {each: 0.1}, opacity: 100, y: 0,}, "<-1"),
 
   }
 )
@@ -116,8 +122,8 @@ const imac_scroller = ScrollTrigger.create(
     animation: gsap.timeline().to(
       imac_icons,
       {y: () => -(imac_icons.scrollHeight * .6)}),
-    preventOverlaps: true,
-    fastScrollEnd: true,
+    // preventOverlaps: true,
+    // fastScrollEnd: true,
     pinSpacing: false,
 
 
@@ -150,7 +156,7 @@ const portfolio_tl = gsap.timeline({
 
 const hm_tl = ScrollTrigger.create(
   {
-    preventOverlaps: true,
+    // preventOverlaps: true,
     trigger: "#hm-section",
     pin: true,
     start: "top top",
@@ -174,7 +180,7 @@ const hm_scrub = gsap.timeline({
     // pin: true,
     scrub: .5,
     trigger: "#hm-section",
-    start: "top-=15vh top",
+    start: "top top",
     end: "center top",
 
   }
@@ -220,8 +226,8 @@ const contact_trigger = ScrollTrigger.create({
   start: "top center",
   toggleActions: "play complete none reverse",
 
-  animation: gsap.timeline().from(
-    ".contact-section-heading", {opacity: 0, x: -10, duration: 1})
+  animation: gsap.timeline().fromTo(
+    ".contact-section-heading", {opacity: 0, x: -10, duration: 1}, {opacity: 1, x: 0, duration: 1})
     .from(".social-icon", {
       duration: .5, scale: 0, y: 20, x: 20, ease: "power1.inOut",
       stagger: {
